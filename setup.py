df0401c8b4c88e822e8c725e050ba48da6fac139
fast240z <fast240z@gmail.com>
2025-05-04 21:57:31 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     author_email='scott.sturdivant@gmail.com',
     license='MIT',
     classifiers=CLASSIFIERS,
-    url='https://github.com/ScottSturdivant/rpi_metar',
+    url='https://github.com/fast240z/rpi_metar',
     long_description=read_md('README.md'),
     long_description_content_type='text/markdown',
     install_requires=open('requirements.txt', 'r').read(),
